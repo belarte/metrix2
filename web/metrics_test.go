7c0554a373e1710d472727412ad627d8458a4a27
Bruno Belarte <bruno.belarte@gmail.com>
2025-06-21 15:53:54 +0200

    refactor tests to check for user interactions rather than implementation details

--- web/metrics_test.go
+++ web/metrics_test.go
@@ -4,46 +4,21 @@ import (
 	"testing"
 
 	"github.com/playwright-community/playwright-go"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
-// MetricsPage models the metric management page for Playwright tests.
-type MetricsPage struct {
-	page playwright.Page
-}
-
-func (mp *MetricsPage) SelectMetric(name string) error {
-	_, err := mp.page.SelectOption("#metric-select", playwright.SelectOptionValues{Values: &[]string{name}})
-	return err
-}
-
-func (mp *MetricsPage) GetTitleValue() (string, error) {
-	return mp.page.InputValue("#metric-title")
-}
-
-func (mp *MetricsPage) GetUnitValue() (string, error) {
-	return mp.page.InputValue("#metric-unit")
-}
-
-func (mp *MetricsPage) GetDescriptionValue() (string, error) {
-	return mp.page.InputValue("#metric-description")
-}
+var pwa = playwright.NewPlaywrightAssertions()
 
 func TestSelectMetricShowsFields(t *testing.T) {
 	env := setupTestEnv(t, "http://localhost:8080/metrics")
 	defer env.teardown()
 
-	metricsPage := &MetricsPage{page: env.page}
+	err := pwa.Locator(env.page.GetByLabel("Title")).ToHaveValue("Weight")
+	assert.NoError(t, err, "expected title field to have value 'Weight'")
 
-	title, err := metricsPage.GetTitleValue()
-	require.NoError(t, err, "could not get title value")
-	require.NotEmpty(t, title, "expected title input to be present and non-empty")
+	err = pwa.Locator(env.page.GetByLabel("Unit")).ToHaveValue("kg")
+	assert.NoError(t, err, "expected unit field to have value 'kg'")
 
-	unit, err := metricsPage.GetUnitValue()
-	require.NoError(t, err, "could not get unit value")
-	require.NotEmpty(t, unit, "expected unit input to be present and non-empty")
-
-	desc, err := metricsPage.GetDescriptionValue()
-	require.NoError(t, err, "could not get description value")
-	require.NotEmpty(t, desc, "expected description textarea to be present and non-empty")
+	err = pwa.Locator(env.page.GetByLabel("Description")).ToHaveValue("Body weight in kilograms")
+	assert.NoError(t, err, "expected description field to have correct value")
 }
