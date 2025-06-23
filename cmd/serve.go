bfa6c0457a7cd9f40757c3abd4d70662288ebf7d
Bruno Belarte <bruno.belarte@gmail.com>
2025-06-23 20:45:28 +0200

    refactor: graceful shutdown

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/belarte/metrix2/router"
 	"github.com/spf13/cobra"
 )
@@ -12,8 +9,7 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Start the web server",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Starting web server on :8080...")
-		log.Fatal(http.ListenAndServe(":8080", router.New()))
+		router.Serve(":8080")
 	},
 }
 
--- router/router.go
+++ router/router.go
@@ -1,7 +1,13 @@
 package router
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/belarte/metrix2/handlers"
 )
@@ -11,4 +17,30 @@ func New() http.Handler {
 	mux.HandleFunc("/", handlers.Home)
 	mux.HandleFunc("/metrics", handlers.Metrics)
 	return mux
+}
+
+func Serve(addr string) {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: New(),
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Println("Starting web server on", addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	<-quit
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+	log.Println("Server exited cleanly.")
 }
